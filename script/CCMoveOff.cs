057970e335a51bd08c313450db8da606a8ec11e6
PeanutADi <37295205+PeanutADi@users.noreply.github.com>
2018-04-10 17:12:11 +0800

    牧师与魔鬼源码及视频

---
+++ script/CCMoveOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCMoveOff : SSAction {
+
+	public UserAction Uaction;
+
+	public string obj;
+
+	public void setObj(string kind){
+		Uaction = SDirector.getInstance ().currentSceneController as UserAction;
+		this.obj = kind;
+	}
+
+	public void getOff () {
+		if (obj == "priest") {
+			Uaction.priestGetOff ();
+		} else if (obj == "devil") {
+			Uaction.devilGetOff ();
+		}
+	}
+}
---
+++ script/SSAction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSAction : MonoBehaviour {
+
+	public void Free(){
+		Destroy (this);
+	}
+}
